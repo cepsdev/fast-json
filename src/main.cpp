241d00add00636b1f1824d29e909b5da5375c4a5
Tomas Prerovsky <cepsdev@hotmail.com>
2025-09-30 18:18:03 +0200

    Slight improvements of the main example.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,17 @@ using namespace std;
 int main(int argc, char** argv){
     Arena<1> arena_allocator;
     fast_json<Arena<1>> jsn;
-    auto r {jsn.read("[1,2,3,4,5,6]",&arena_allocator,0)};
+    string json = "[1,2,3,4,5,6]";
+    auto r {jsn.read(json,&arena_allocator,0)};
     if(r){
         double sum{};
         traverse_binary_representation(r->first, r->second.second, [&sum](msg_node*m, bool enter){
             if (m->what== msg_node::F64) sum += ((msg_node_f64*)m)->value;
         });
         cout << " sum = " << sum << '\n';
-
     } else {
-        cout << "Failed to read JSON.\n";
+        cerr << "Failed to read JSON.\n";
+        return 1;
     }
     return 0;
 }
